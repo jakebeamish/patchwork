e5f3bb2bfea664c1533d1b9d8d15d7e211975111
Jake Beamish <jakebeamish@gmail.com>
2022-11-02 12:11:36 +0000

    Tweak

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let boxWidth, boxHeight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // createCanvas(5000, 5000)
   frameRate(30);
   background((round(random()) * 255));
 }
@@ -9,23 +10,31 @@ function setup() {
 function draw() {
   strokeWeight(random(3));
 
-  if (random() < 0.4) {
-    for (let i = 0; i < random(1000); i++) {
+  if (random() < 0.1) {
+    for (let i = 0; i < random(1000,10000); i++) {
       point(random(width), random(height))
     }
   }
 
-  boxWidth = random(width / 10, width / 2);
-  boxHeight = random(height / 10, height / 2);
+  // if (random() < 0.4) {
+  //   strokeWeight(random(10, 50));
+  //   let y = random(height);
+  //   line(0, y, width, y);
+  // }
+
+    strokeWeight(random(3));
+
+  boxWidth = random(width / 20, width / 2);
+  boxHeight = random(height / 20, height / 2);
 
   for (let y = 0; y <= height; y += boxHeight) {
     for (let x = 0; x <= width; x += boxWidth) {
-      if (random() < 0.01) {
+      if (random() < 0.1) {
         rotate(random(PI))
       };
 
       // Draw stippled blocks
-      if (random() > 0.5) {
+      if (random() < 0.7) {
         stipple(
           x,
           x + 2 * random(boxWidth / 3),
@@ -55,7 +64,7 @@ function draw() {
         circle(x, y, boxWidth * 2)
       }
 
-      if (random() < 0.01) {
+      if (random() < 0.005) {
         if (random() < 0.5) {
           // Decide fill colour (50%)
           fill(round(random()) * 255);
@@ -70,18 +79,21 @@ function draw() {
       }
 
       // Draw perpendicular lines
-      if (random() < 0.01) {
+      if (random() < 0.05) {
+        push();
+        strokeWeight(random(1, 50))
         line(x, 0, x, height);
         line(0, y, width, y);
+        pop();
       }
 
       // Draw psuedotext
-      if (random() < 0.2) {
+      if (random() < 0.4) {
         strokeCap(PROJECT);
         strokeWeight(random(5));
         stroke(round(random()) * 255);
-        let xsize = pow(random(10), 3);
-        let ysize = pow(random(10), 3);
+        let xsize = pow(random(10), 2);
+        let ysize = pow(random(10), 2);
         minMarks = 3;
         maxMarks = random(20);
         spaces = 0.4;
@@ -115,10 +127,10 @@ function stipple(xmin, xmax, ymin, ymax, dots) {
   }
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  background((round(random()) * 255));
-}
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+//   background((round(random()) * 255));
+// }
 
 function drawCharacter(xsize, ysize) {
   if (random() < spaces) return;
